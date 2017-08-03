f2a423ab15a8aa78f929691a09e7a656fde3667c
Bernard Strong <bstrong71@gmail.com>
2017-08-03 16:50:01 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// Writing the JavaScript and interacting with the DOM is the main part of the project
+// Start small by selecting some elements
+// What algorithms do you need to implement?
+// Create some functions to store repeatable steps
+// You'll need to find a way to track the numbers pressed until the = is pressed
+// No need for chaining long calculations yet (see hard mode)
