32beb7b103119294d6cd557dcc8eb7b03386b889
Bernard Strong <bstrong71@gmail.com>
2017-08-04 14:27:06 -0400

    numbers assigned to buttons

--- main.js
+++ main.js
@@ -4,3 +4,23 @@
 // Create some functions to store repeatable steps
 // You'll need to find a way to track the numbers pressed until the = is pressed
 // No need for chaining long calculations yet (see hard mode)
+// Button assignments
+let cButton = document.getElementById('cButton');
+let num7 = Number(document.getElementById('num7').textContent);
+let num8 = Number(document.getElementById('num8').textContent);
+let num9 = Number(document.getElementById('num9').textContent);
+let divButton = document.getElementById('divButton').textContent;
+let num4 = Number(document.getElementById('num4').textContent);
+let num5 = Number(document.getElementById('num5').textContent);
+let num6 = Number(document.getElementById('num6').textContent);
+let multButton = document.getElementById('multButton').textContent;
+let num1 = Number(document.getElementById('num1').textContent);
+let num2 = Number(document.getElementById('num2').textContent);
+let num3 = Number(document.getElementById('num3').textContent);
+let subtrButton = document.getElementById('subtrButton').textContent;
+let num0 = Number(document.getElementById('num0').textContent);
+let decimal = document.getElementById('decimal').textContent;
+let eqButton = document.getElementById('eqButton').textContent;
+let plusButton = document.getElementById('plusButton').textContent;
+
+console.log(num7 + num9 + num1 + num2);
